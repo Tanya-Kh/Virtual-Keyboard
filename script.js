fb6e5caf9938aaac9830194ce2f37cd6e1c3f21d
Tanya Khreshchykava <tanya.khreshchykava@gmail.com>
2020-04-08 00:00:54 -0400

    feat: add language change

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 ﻿const wrapper = document.createElement("div");
 const keyboard = document.createElement("div");
 const textarea = document.createElement("textarea");
+let langEn = true;
 
 const keys = [
 	'`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del', 'Caps Lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter', 'Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '&#9650', 'Shift', 'Ctrl', 'Win', 'Alt', ' ', 'Alt', '&#9668', '&#9660', '&#9658', 'Ctrl'
@@ -8,6 +9,13 @@ const keys = [
 
 const keyCodes = [
 	'Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace', 'Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Delete', 'CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter', 'ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight', 'ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight'
+];
+
+const russianKeys = [
+	'ё', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace', 'Tab', 'й', 'ц', 'у', 'к',
+	'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '/', 'Del', 'Caps Lock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д',
+	'ж', 'э', 'Enter', 'Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '&#9650', 'Shift', 'Ctrl', 'Win', 'Alt',
+	' ', 'Alt', '&#9668', '&#9660', '&#9658', 'Ctrl'
 ];
 
 document.body.onload = init();
@@ -38,7 +46,6 @@ function addKeyboard() {
 }
 
 function addKeys() {
-
 	for (let i = 0; i < keys.length; i++) {
 		let button = document.createElement("button");
 		button.innerHTML = keys[i];
@@ -66,10 +73,17 @@ function addNotes() {
 	wrapper.appendChild(notes);
 }
 
+let keysPressed = [];
+
 document.addEventListener("keydown", function (event) {
 
 	if (document.getElementById(event.code)) {
 		document.getElementById(event.code).classList.add("key-active");
+		keysPressed.push(event.code);
+		if ((keysPressed[0] == "ShiftLeft" && event.code == "AltLeft") ||
+			(keysPressed[0] == "AltLeft" && event.code == "ShiftLeft")) {
+			changeLang();
+		}
 	}
 });
 
@@ -77,5 +91,30 @@ document.addEventListener("keyup", function (event) {
 
 	if (document.getElementById(event.code)) {
 		document.getElementById(event.code).classList.remove("key-active");
+		keysPressed.length = 0;
 	}
 });
+
+function changeLang() {
+	langEn == true ? langEn = false : langEn = true;
+	changeKeys();
+}
+
+function changeKeys() {
+	let alphabet;
+	let keyboardKeys = document.getElementsByClassName("keyboard")[0];
+
+	if (langEn) {
+		alphabet = keys;
+	} else {
+		alphabet = russianKeys;
+	}
+
+	for (let i = 0; i < alphabet.length; i++) {
+		let button = keyboardKeys.childNodes[i];
+		button.innerHTML = alphabet[i];
+	}
+
+}
+
+
